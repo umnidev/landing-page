76b1247caae6bd36848c79bdec7a2aea5e02a6cd
knutole <knutole@mapic.io>
2023-03-26 13:22:46 +0800

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://umnidev.github.io",
-  base: "/landing-page",
   integrations: [
     tailwind(),
     image({
