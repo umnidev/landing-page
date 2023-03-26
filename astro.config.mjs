acdffdb8fda1efd6f2152f43fe12a2847c3304a7
knutole <knutole@mapic.io>
2023-03-26 14:54:12 +0800

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://umnidev.github.io",
+  base: "/landing-page/",
   // srcDir: ".",
   integrations: [
     tailwind(),
