2d2125bdd0929efa82083a7094b4ee0ec9231680
knutole <knutole@mapic.io>
2023-03-26 15:31:08 +0800

    update title

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://umnidev.github.io/landing-page/",
-  // srcDir: ".",
   integrations: [
     tailwind(),
     image({
